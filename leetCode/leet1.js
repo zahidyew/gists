c9d07323ceb08563071f45a71f992bec85450573
zahidyew <zahidyew96@gmail.com>
2021-01-28 18:24:19 +0800

    created new folder & file for leetcode problems

---
+++ leetCode/leet1.js
@@ -0,0 +1,35 @@
+/* var twoSum = function (nums, target) {
+   const len = nums.length
+
+   for (let i = 0; i < len; i++) {
+      let x = target - nums[i]
+      //console.log(i)
+
+      if (nums.lastIndexOf(x) !== -1 && nums.lastIndexOf(x) !== i) {
+         return [i, nums.lastIndexOf(x)]
+      }
+   }
+   return 0
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)) */
+// [2,7,11,15], 9
+// [3,3], 6
+// [3, 2, 4], 6
+
+
+var twoSum = function (nums, target) {
+   const len = nums.length
+   let arry = []
+
+   for (let i = 0; i < len; i++) {
+      if (arry[nums[i]] >= 0) {
+         return [arry[nums[i]], i]
+      }
+      let x = target - nums[i]
+      arry[x] = i
+   }
+   return 0
+};
+
+console.log(twoSum([3, 2, 4], 6))
