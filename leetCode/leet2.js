8ee101972ad6e5cd8b18388f0e3366066eac01e9
zahidyew <zahidyew96@gmail.com>
2021-03-12 01:12:01 +0800

    update ini to remove dependabot alert

--- leetCode/leet2.js
+++ leetCode/leet2.js
@@ -1,17 +1,17 @@
 // 217. Contains Duplicate
 const containsDuplicate = (nums) => {
-   const n = nums.length
-   const obj = {}
+   const n = nums.length;
+   const obj = {};
 
    for (let i = 0; i < n; i++) {
       if (obj[nums[i]] !== 1) {
-         obj[nums[i]] = 1
+         obj[nums[i]] = 1;
       }
       else {
-         return true
+         return true;
       }
    }
-   return false
+   return false;
 
    /* 
    const n = nums.length
@@ -27,103 +27,103 @@ const containsDuplicate = (nums) => {
    }
    return false
     */
-}
+};
 //console.log(containsDuplicate([1, 2, 3, 1]))
 
 
 // 1446. Consecutive Characters
 const maxPower = (s) => {
-   let back = 0
-   let front = 1
-   let max = 1
-   let c = 1
+   let back = 0;
+   let front = 1;
+   let max = 1;
+   let c = 1;
 
-   while(front < s.length) {
+   while (front < s.length) {
       if (s[back] == s[front]) {
-         console.log(s[back] + " == " + s[front])
-         front++
-         c++
+         console.log(s[back] + " == " + s[front]);
+         front++;
+         c++;
       }
       else {
          //console.log('asfasf')
          if (c > max) {
-            max = c
+            max = c;
          }
-         c = 1
-         back = front
-         front++
+         c = 1;
+         back = front;
+         front++;
       }
    }
    // sometimes the else part didnt run (eg: "aaa"),
    // so it didnt update max value yet. So, we simply
    // compare max & c before returning & return the 
    // largest value btween the 2.
-   return Math.max(max, c)
-}
+   return Math.max(max, c);
+};
 //console.log(maxPower('ccaaa'))
 
 
 // 896. Monotonic Array
 const isMonotonic = (A) => {
-   let ascend = false
-   let descend = false
+   let ascend = false;
+   let descend = false;
 
    for (let i = 0; i < A.length; i++) {
-      if (A[i] === A[i+1]) {
+      if (A[i] === A[i + 1]) {
          continue;
-      }  
-      else if (A[i] > A[i+1]) {
-         descend = true
+      }
+      else if (A[i] > A[i + 1]) {
+         descend = true;
          //console.log('des' + i)
       }
-      else if (A[i] < A[i+1]) {
-         ascend = true
+      else if (A[i] < A[i + 1]) {
+         ascend = true;
          //console.log('asc' + i)
       }
       if (ascend && descend) {
-         return false
+         return false;
       }
    }
-   return true
-}
+   return true;
+};
 //console.log(isMonotonic([2, 2, 2, 1, 4, 5]))
 
 
 // 383. Ransom Note
 var canConstruct = function (ransomNote, magazine) {
-   let rn = {}
-   let mag = {}
-   let flag = true
+   let rn = {};
+   let mag = {};
+   let flag = true;
    //ransomNote = ransomNote.trim()
    //magazine = magazine.trim()
 
    for (let i = 0; i < ransomNote.length; i++) {
       if (rn[ransomNote[i]] > 0) {
-         rn[ransomNote[i]]++
+         rn[ransomNote[i]]++;
       }
       else {
-         rn[ransomNote[i]] = 1
+         rn[ransomNote[i]] = 1;
       }
    }
 
    magazine.split("").map(letter => {
       if (mag[letter] > 0) {
-         mag[letter]++
+         mag[letter]++;
       }
       else {
-         mag[letter] = 1
+         mag[letter] = 1;
       }
-   })
+   });
 
    for (let [key] in rn) {
       if (rn[key] <= mag[key]) {
-         flag = true
+         flag = true;
       }
       else {
-         return false
+         return false;
       }
    }
-   return flag
+   return flag;
 };
 //console.log(canConstruct("", ""))
 
@@ -142,10 +142,10 @@ var distributeCandies = function (candyType) {
       return type
    } */
 
-   const n = candyType.length / 2
-   const type = new Set(candyType).size
+   const n = candyType.length / 2;
+   const type = new Set(candyType).size;
 
-   return type >= n ? n : type
+   return type >= n ? n : type;
 };
 //console.log(distributeCandies([1, 1, 2, 2, 3, 3]))
 
@@ -184,55 +184,55 @@ var slowestKey = function (releaseTimes, keysPressed) {
 
    return keysPressed[index[0]] */
 
-   let max = releaseTimes[0]
-   let ans = keysPressed[0]
+   let max = releaseTimes[0];
+   let ans = keysPressed[0];
 
    for (let i = 1; i < releaseTimes.length; i++) {
-      let d = releaseTimes[i] - releaseTimes[i - 1]
+      let d = releaseTimes[i] - releaseTimes[i - 1];
 
       if (d > max) {
-         max = d
-         ans = keysPressed[i]
+         max = d;
+         ans = keysPressed[i];
       }
       // if same time, we want the lexicographically larger key
       else if (d === max && keysPressed[i] > ans) {
-         ans = keysPressed[i]
+         ans = keysPressed[i];
       }
    }
 
-   return ans
+   return ans;
 };
 //console.log(slowestKey([3, 6, 8, 10, 16, 17, 26, 41, 72, 88], "uzhnanabvy"))
 
 
 // 7. Reverse Integer 
 var reverse = function (x) {
-   let ans
+   let ans;
    if (x < 0) {
-      x = x.toString()
-      ans = "-" + rev(x, 0)
+      x = x.toString();
+      ans = "-" + rev(x, 0);
    }
    else {
-      x = x.toString()
-      ans = rev(x, -1)
+      x = x.toString();
+      ans = rev(x, -1);
    }
-   ans = parseInt(ans)
+   ans = parseInt(ans);
    if (ans > 2147483647 || ans < -2147483648) {
-      return 0
+      return 0;
    }
    else {
-      return ans
+      return ans;
    }
 };
 
-function rev (x, end) {
-   let r = ""
+function rev(x, end) {
+   let r = "";
 
    for (let i = x.length - 1; i > end; i--) {
-      r = r + x[i]
+      r = r + x[i];
    }
-   console.log(r)
-   return r
+   console.log(r);
+   return r;
 }
 //console.log(reverse(2147483647))
 
@@ -248,17 +248,17 @@ var isPalindrome = function (x) {
    return rev == x ? true : false */
 
    if (x < 0 || x % 10 === 0 && x !== 0) {
-      return false
+      return false;
    }
 
-   let rev = 0
+   let rev = 0;
    while (x > rev) {
-      rev = rev * 10 + x % 10
-      x = x / 10
-      console.log(rev)
+      rev = rev * 10 + x % 10;
+      x = x / 10;
+      console.log(rev);
    }
 
-   return x === rev || x === rev/10
+   return x === rev || x === rev / 10;
 };
 //console.log(isPalindrome(121))
 
@@ -266,7 +266,7 @@ var isPalindrome = function (x) {
 // 13. Roman to Integer
 var romanToInt = function (s) {
    let ans = 0;
-   const eCases = s.match(/(?:IV|IX|XL|XC|CD|CM)/g)
+   const eCases = s.match(/(?:IV|IX|XL|XC|CD|CM)/g);
    const romans = {
       "I": 1,
       "V": 5,
@@ -281,14 +281,14 @@ var romanToInt = function (s) {
       "XC": 90,
       "CD": 400,
       "CM": 900,
-   }
+   };
 
    // deal with edge cases in eCases
    if (eCases !== null) {
       for (let i = 0; i < eCases.length; i++) {
          // add up all the captured edge cases & remove it from the string
-         ans = ans + romans[eCases[i]]
-         s = s.replace(eCases[i], "")
+         ans = ans + romans[eCases[i]];
+         s = s.replace(eCases[i], "");
 
          //console.log(ans)
          //console.log(s)
@@ -297,10 +297,10 @@ var romanToInt = function (s) {
 
    // add up the all the simple romans
    s.split("").map(r => {
-      ans = ans + romans[r]
-   })
+      ans = ans + romans[r];
+   });
 
-   return ans
+   return ans;
 };
 //console.log(romanToInt('MMXIX'))
 
@@ -308,20 +308,20 @@ var romanToInt = function (s) {
 // 14. Longest Common Prefix
 var longestCommonPrefix = function (strs) {
    if (strs.length === 0) {
-      return ""
+      return "";
    }
 
-   let ans = strs[0]
+   let ans = strs[0];
 
    for (let i = 1; i < strs.length; i++) {
       while (strs[i].indexOf(ans) !== 0) {
-         ans = ans.substring(0, ans.length - 1)
+         ans = ans.substring(0, ans.length - 1);
          if (ans.length === 0) {
-            return ""
+            return "";
          }
       }
    }
-   return ans
+   return ans;
 };
 //console.log(longestCommonPrefix(["flower", "flow", "flight"]))
 
@@ -341,18 +341,18 @@ var removeDuplicates = function (nums) {
    return l */
 
    if (nums.length < 2) {
-      return nums.length
+      return nums.length;
    }
 
-   let i = 0
+   let i = 0;
 
    for (let x = 1; x < nums.length; x++) {
       if (nums[i] !== nums[x]) {
-         i++
-         nums[i] = nums[x]
+         i++;
+         nums[i] = nums[x];
       }
    }
-   return i + 1
+   return i + 1;
 };
 //console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 
@@ -360,10 +360,10 @@ var removeDuplicates = function (nums) {
 // 27. Remove Element
 var removeElement = function (nums, val) {
    while (nums.indexOf(val) !== -1) {
-      nums.splice(nums.indexOf(val), 1)
+      nums.splice(nums.indexOf(val), 1);
    }
    //return nums.length
-   return nums
+   return nums;
 };
 //console.log(removeElement([3, 2, 2, 3], 3))
 
@@ -372,26 +372,26 @@ var removeElement = function (nums, val) {
 var searchInsert = function (nums, target) {
    // if target is inside the array, return its position immediately
    if (nums.indexOf(target) !== -1) {
-      return nums.indexOf(target)
+      return nums.indexOf(target);
    }
    // else find where it should be inserted
    else {
-      const max = Math.max(...nums)
-      const min = Math.min(...nums)
+      const max = Math.max(...nums);
+      const min = Math.min(...nums);
 
       if (target > max) {
          // insert at the end
-         return nums.length
+         return nums.length;
       }
       else if (target < min) {
          // insert at the beginning
-         return 0
+         return 0;
       }
 
       // target belong somewhere in the middle
       for (let i = 1; i < nums.length; i++) {
          if (nums[i - 1] < target && nums[i] > target) {
-            return i
+            return i;
          }
       }
    }
@@ -402,7 +402,7 @@ var searchInsert = function (nums, target) {
 
       if (i === (nums.length - 1) && target > nums[i]) return i + 1;
    } */
-   
+
 };
 //console.log(searchInsert([1, 3, 5, 6], 2))
 
@@ -410,32 +410,32 @@ var searchInsert = function (nums, target) {
 // 278. First Bad Version
 var solution = function (isBadVersion) {
    return function (n) {
-      let left = 1
-      let right = n
+      let left = 1;
+      let right = n;
 
       // binary search 
       while (left < right) {
-         let mid = Math.floor(left + (right - left) / 2)
+         let mid = Math.floor(left + (right - left) / 2);
 
          if (isBadVersion(mid)) {
-            right = mid
+            right = mid;
          } else {
-            left = mid + 1
+            left = mid + 1;
          }
          /* console.log("mid " + mid)
          console.log("n " + n)
          console.log("right " + right)
          console.log("left " + left + "\n") */
       }
-      return left
+      return left;
    };
 };
 function isBadVersion(n) {
-   const badVersion = 2
+   const badVersion = 2;
    if (n >= badVersion) {
-      return true
+      return true;
    } else {
-      return false
+      return false;
    }
 }
 //console.log(solution(isBadVersion)(10))
@@ -443,30 +443,30 @@ function isBadVersion(n) {
 
 // 1480. Running Sum of 1d Array
 var runningSum = function (nums) {
-   let sum = [nums[0]]
+   let sum = [nums[0]];
 
    if (nums.length > 1) {
       for (let i = 1; i < nums.length; i++) {
-         sum[i] = sum[i - 1] + nums[i]
+         sum[i] = sum[i - 1] + nums[i];
       }
    }
-   return sum
+   return sum;
 };
 //console.log(runningSum([3, 1, 2, 10, 1]))
 
 
 // 1672. Richest Customer Wealth
 var maximumWealth = function (accounts) {
-   let max = 0
+   let max = 0;
 
    for (let i = 0; i < accounts.length; i++) {
-      let wealth = accounts[i].reduce((a, b) => a + b)
+      let wealth = accounts[i].reduce((a, b) => a + b);
 
       if (wealth > max) {
-         max = wealth
+         max = wealth;
       }
    }
-   return max
+   return max;
 
    //return Math.max(...accounts.map(customer => customer.reduce((a, b) => a + b)))
 };
@@ -492,20 +492,20 @@ var numJewelsInStones = function (jewels, stones) {
    } else {
       return 0
    } */
-   
+
    //let ans = []
-   let n = 0
-   
+   let n = 0;
+
    for (let i in jewels) {
-      const myRegex = new RegExp(jewels[i], "g")
-      const matched = stones.match(myRegex)
+      const myRegex = new RegExp(jewels[i], "g");
+      const matched = stones.match(myRegex);
       if (matched !== null) {
          //ans.push(matched)
-         n = n + matched.length
+         n = n + matched.length;
       }
    }
-    
-   return n
+
+   return n;
 
    /* let n = 0
    for (let i = 0; i < stones.length; i++) {
@@ -520,94 +520,94 @@ var numJewelsInStones = function (jewels, stones) {
 
 // 1342. Number of Steps to Reduce a Number to Zero
 var numberOfSteps = function (num) {
-   let count = 0
+   let count = 0;
 
    while (num > 0) {
       if (num % 2 === 0) {
-         num = num / 2
+         num = num / 2;
       } else {
-         num = num - 1
+         num = num - 1;
       }
-      count++
+      count++;
    }
-   return count
+   return count;
 };
 //console.log(numberOfSteps(14))
 
 
 // 1313. Decompress Run-Length Encoded List
 var decompressRLElist = function (nums) {
-   let ans = []
+   let ans = [];
 
    for (let i = 0; i < nums.length; i += 2) {
-      const freq = nums[i]
-      const val = nums[i + 1]
-      ans.push(...new Array(freq).fill(val))
+      const freq = nums[i];
+      const val = nums[i + 1];
+      ans.push(...new Array(freq).fill(val));
 
       //for (let j = 0; j < freq; j++) {
       //ans.push(val)
       //}
    }
-   return ans
+   return ans;
 };
 
 
 // 242. Valid Anagram
 var isAnagram = function (s, t) {
-   let lettersInS = {}
-   let lettersInT = {}
+   let lettersInS = {};
+   let lettersInT = {};
 
    s.toLowerCase().split("").map(l => {
       if (lettersInS[l] > 0) {
-         lettersInS[l]++
+         lettersInS[l]++;
       } else {
-         lettersInS[l] = 1
+         lettersInS[l] = 1;
       }
       //console.log(lettersInS[l])
-   })
+   });
 
    t.toLowerCase().split("").map(l => {
       if (lettersInT[l] > 0) {
-         lettersInT[l]++
+         lettersInT[l]++;
       } else {
-         lettersInT[l] = 1
-      } 
-   })
+         lettersInT[l] = 1;
+      }
+   });
 
-   console.log(lettersInS)
-   console.log(lettersInT)
+   console.log(lettersInS);
+   console.log(lettersInT);
 
    if (Object.keys(lettersInS).length !== Object.keys(lettersInT).length) {
-      return false
+      return false;
    }
 
    for (let i in lettersInS) {
       if (lettersInS[i] !== lettersInT[i]) {
-         return false
+         return false;
       }
    }
-   return true
+   return true;
 };
 //console.log(isAnagram("evil", "vile"))
 
 
 // 53. Maximum Subarray (Can be solved using Kadane's Algorithm)
 var maxSubArray = function (nums) {
-   let sum = 0
-   let max = Number.MIN_SAFE_INTEGER
+   let sum = 0;
+   let max = Number.MIN_SAFE_INTEGER;
 
    for (let i = 0; i < nums.length; i++) {
       if (nums[i] > sum + nums[i]) {
-         sum = nums[i]
+         sum = nums[i];
       } else {
-         sum = sum + nums[i]
+         sum = sum + nums[i];
       }
       if (sum > max) {
-         max = sum
+         max = sum;
       }
    }
 
-   return max
+   return max;
 };
 
 
@@ -617,8 +617,8 @@ var lengthOfLastWord = function (s) {
    const lastSpaceIndex = s.lastIndexOf(" ")
    return s.length - lastSpaceIndex - 1 */
 
-   s = s.trim()
-   return s.length - s.lastIndexOf(" ") - 1
+   s = s.trim();
+   return s.length - s.lastIndexOf(" ") - 1;
 };
 
 
@@ -677,36 +677,36 @@ var plusOne = function (digits) {
 
 // 67. Add Binary
 var addBinary = function (a, b) {
-   let addOne = 0
-   let ans = ""
-   let i = a.length - 1
-   let j = b.length - 1
-   
+   let addOne = 0;
+   let ans = "";
+   let i = a.length - 1;
+   let j = b.length - 1;
+
    while (i > -1 || j > -1 || addOne !== 0) {
-      let sum = 0
+      let sum = 0;
       if (a[i] === undefined && b[j] === undefined) {
-         sum = addOne
+         sum = addOne;
       } else if (a[i] === undefined) {
-         sum = parseInt(b[j]) + addOne
+         sum = parseInt(b[j]) + addOne;
       } else if (b[j] === undefined) {
-         sum = parseInt(a[i]) + addOne
+         sum = parseInt(a[i]) + addOne;
       } else {
-         sum = parseInt(a[i]) + parseInt(b[j]) + addOne
+         sum = parseInt(a[i]) + parseInt(b[j]) + addOne;
       }
 
       if (sum > 1) {
-         ans = (sum - 2) + ans // concatenate string
-         addOne = 1
+         ans = (sum - 2) + ans; // concatenate string
+         addOne = 1;
       } else {
-         ans = sum + ans
-         addOne = 0
+         ans = sum + ans;
+         addOne = 0;
       }
       //console.log(ans)
       //console.log(sum)
-      i--
-      j--
+      i--;
+      j--;
    }
-   return ans
+   return ans;
 
    /*
    a = `0b${a}`;
@@ -797,7 +797,7 @@ var judgeCircle = function (moves) {
 var smallerNumbersThanCurrent = function (nums) {
    let ans = new Array(...nums);
    let obj = {};
-   ans = ans.sort((a,b) => a - b);
+   ans = ans.sort((a, b) => a - b);
    //console.log(ans)
 
    for (let i = 0; i < ans.length; i++) {
@@ -828,32 +828,32 @@ function calculateDaysSince() {
 
 
 // 3. Longest Substring Without Repeating Characters
-var lengthOfLongestSubstring = function(s) {
-    let start = 0;
-    let end = start + 1;
-    let chars = {};
-    let maxLen = 0;
+var lengthOfLongestSubstring = function (s) {
+   let start = 0;
+   let end = start + 1;
+   let chars = {};
+   let maxLen = 0;
 
-    if (s.length === 0) { return 0; }
+   if (s.length === 0) { return 0; }
 
-    while (end < s.length) {
-        chars[s[start]] = 1;
+   while (end < s.length) {
+      chars[s[start]] = 1;
 
-        if (chars[s[end]] === undefined) {
-            chars[s[end]] = 1;
-            end++;
-        } else {
-            let len = end - start;
-            if (len > maxLen) {
-                maxLen = len;
-            }
-            start = start + 1;
-            end = start + 1;
-            chars = {};
-        }
-        //console.log(chars);
-    }
-    return Math.max(maxLen, end-start);
+      if (chars[s[end]] === undefined) {
+         chars[s[end]] = 1;
+         end++;
+      } else {
+         let len = end - start;
+         if (len > maxLen) {
+            maxLen = len;
+         }
+         start = start + 1;
+         end = start + 1;
+         chars = {};
+      }
+      //console.log(chars);
+   }
+   return Math.max(maxLen, end - start);
 
    /* let hash = new Map();
    let max = 0;
@@ -928,7 +928,7 @@ var reverseWords = function (s) {
    }
    return reversed;
 };
-console.log(reverseWords("  Bob    Loves  Alice   "))
+console.log(reverseWords("  Bob    Loves  Alice   "));
 
 
 /* - Given an array of integers and a target integer, return true if the array contains 2 integers that sum up to target
@@ -936,16 +936,16 @@ Input: [3, 2, 1, 4, 5, 6], target = 10
 Output: true
 
 function sum(input, target) {
-	let obj = {};
+   let obj = {};
 
   for (int i = 0; i < input.length; i++) {
-  	obj[input[i]] = input[i]
+       obj[input[i]] = input[i]
   }
 
-	for (int i = 0; i < input.length; i++) {
-		int diff = target - input[i];
+   for (int i = 0; i < input.length; i++) {
+      int diff = target - input[i];
     if (obj[diff] != null &&) {
-    	return iftrue;
+        return iftrue;
     }
   }
   return false;
@@ -956,20 +956,20 @@ function sum(input, target) {
 
 
 class Node {
-	int value
+   int value
   Node next?
 }
 
 1-2-3-4-5
 5-4-3-2-1- null
 function reverse(Node head) {
-	if (head == null) return head;
+   if (head == null) return head;
 
   Node reversed = null;
   Node next;
 
   while(head != null) {
-  	next = head.next; //3-4-5
+       next = head.next; //3-4-5
     head.next = reversed // 2-1-null
     reversed = head; //2-1-null
     head = next; // 3-4-5
