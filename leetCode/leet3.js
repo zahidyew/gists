48a898a3d84ba8b1a008ad43cb1cfcda42fa8778
zahidyew <zahidyew96@gmail.com>
2021-03-24 00:40:11 +0800

    solved 1 more ques

--- leetCode/leet3.js
+++ leetCode/leet3.js
@@ -40,4 +40,27 @@ var subtractProductAndSum = function (n) {
    }
    return product - sum;
 };
-console.log(subtractProductAndSum(234));
+//console.log(subtractProductAndSum(234));
+
+
+// 1662. Check If Two String Arrays are Equivalent
+var arrayStringsAreEqual = function (word1, word2) {
+   let str1 = "";
+   let str2 = "";
+
+   for (let i = 0; i < word1.length; i++) {
+      str1 = str1 + word1[i];
+   }
+   for (let i = 0; i < word2.length; i++) {
+      str2 = str2 + word2[i];
+   }
+
+   if (str1 === str2) {
+      return true;
+   } else {
+      return false;
+   }
+
+   //return word1.join('') === word2.join('')
+};
+console.log(arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"]));
